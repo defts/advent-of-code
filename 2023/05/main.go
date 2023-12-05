93c74047b0515c32d5e7fcc14074c4b96f43c8a0
David Torres Dias <david@torresdias.fr>
2023-12-05 20:09:07 +0100

    add goroutines for part2

--- 2023/05/main.go
+++ 2023/05/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func computeMap(i int, m [][]int) (o int) {
 	for _, l := range m {
@@ -35,14 +38,40 @@ func main() {
 	fmt.Printf("minLocation: %d\n", minLocation)
 
 	// part 2
+	// minLocation = 0
+	// for i := 0; i < len(seeds); i += 2 {
+	// 	for j := 0; j < seeds[i+1]; j++ {
+	// 		location := computeSeed(seeds[i] + j)
+	// 		if location < minLocation || minLocation == 0 {
+	// 			minLocation = location
+	// 		}
+	// 	}
+	// }
+
+	// with goroutines part2
+	var wg sync.WaitGroup
+	var mu sync.Mutex
 	minLocation = 0
 	for i := 0; i < len(seeds); i += 2 {
-		for j := 0; j < seeds[i+1]; j++ {
-			location := computeSeed(seeds[i] + j)
-			if location < minLocation || minLocation == 0 {
-				minLocation = location
+		wg.Add(1)
+		go func(start, count int) {
+			defer wg.Done()
+
+			localMinLocation := minLocation
+			for j := 0; j < count; j++ {
+				location := computeSeed(start + j)
+				if location < localMinLocation || localMinLocation == 0 {
+					localMinLocation = location
+				}
 			}
-		}
+
+			mu.Lock()
+			defer mu.Unlock()
+			if localMinLocation < minLocation || minLocation == 0 {
+				minLocation = localMinLocation
+			}
+		}(seeds[i], seeds[i+1])
 	}
+	wg.Wait()
 	fmt.Printf("minLocation part2: %d\n", minLocation)
 }
