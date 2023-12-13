bdee1fa63f7065d5627d0912083fc5605a912a14
David Torres Dias <david@torresdias.fr>
2023-12-13 09:34:23 +0100

    2023-12-13 - part2

--- 2023/13/main.go
+++ 2023/13/main.go
@@ -2,9 +2,10 @@ package main
 
 import "fmt"
 
-func findVerticalReflect(pattern [][]rune) int {
+func findVerticalReflect(pattern [][]rune, wantedError int) int {
 p:
 	for col := 0; col < len(pattern[0])-1; col++ {
+		errorFound := 0
 		for a := 0; a <= col; a++ {
 			b := col + col - a + 1
 			if b >= len(pattern[0]) {
@@ -12,20 +13,26 @@ p:
 			}
 			for row := range pattern {
 				if pattern[row][a] != pattern[row][b] {
-					// on sort de la boucle interne
+					if errorFound < wantedError {
+						errorFound++
+						continue
+					}
 					continue p
 				}
 			}
 		}
-		return col + 1
+		if wantedError == errorFound {
+			return col + 1
+		}
 	}
 
 	return -1
 }
 
-func findHorizontalReflect(pattern [][]rune) int {
+func findHorizontalReflect(pattern [][]rune, wantedError int) int {
 p:
 	for row := 0; row < len(pattern)-1; row++ {
+		errorFound := 0
 		for a := 0; a <= row; a++ {
 			b := row + row - a + 1
 			if b >= len(pattern) {
@@ -33,12 +40,17 @@ p:
 			}
 			for col := range pattern[0] {
 				if pattern[a][col] != pattern[b][col] {
-					// on sort de la boucle interne
+					if errorFound < wantedError {
+						errorFound++
+						continue
+					}
 					continue p
 				}
 			}
 		}
-		return row + 1
+		if wantedError == errorFound {
+			return row + 1
+		}
 	}
 
 	return -1
@@ -48,15 +60,30 @@ func main() {
 	loadFile("input.txt")
 	res := 0
 	for _, p := range patterns {
-		h := findHorizontalReflect(p)
+		h := findHorizontalReflect(p, 0)
 		if h != -1 {
 			res += h * 100
 		} else {
-			v := findVerticalReflect(p)
+			v := findVerticalReflect(p, 0)
 			if v != -1 {
 				res += v
 			}
 		}
 	}
 	fmt.Println("part1:", res)
+
+	res = 0
+	for _, p := range patterns {
+		h := findHorizontalReflect(p, 1)
+		if h != -1 {
+			res += h * 100
+		} else {
+			v := findVerticalReflect(p, 1)
+			if v != -1 {
+				res += v
+			}
+		}
+	}
+	fmt.Println("part2:", res)
+
 }
--- 2023/13/main_test.go
+++ 2023/13/main_test.go
@@ -7,7 +7,8 @@ import (
 func Test_findVerticalReflect(t *testing.T) {
 	loadFile("test.txt")
 	type args struct {
-		pattern [][]rune
+		pattern     [][]rune
+		wantedError int
 	}
 	tests := []struct {
 		name string
@@ -15,23 +16,41 @@ func Test_findVerticalReflect(t *testing.T) {
 		want int
 	}{
 		{
-			name: "test vertical pattern",
+			name: "test vertical pattern - part 1",
 			args: args{
-				pattern: patterns[0],
+				patterns[0],
+				0,
 			},
 			want: 5,
 		},
 		{
-			name: "test vertical pattern",
+			name: "test vertical pattern - part 1",
 			args: args{
-				pattern: patterns[1],
+				patterns[1],
+				0,
+			},
+			want: -1,
+		},
+		{
+			name: "test vertical pattern - part 2",
+			args: args{
+				patterns[0],
+				1,
+			},
+			want: -1,
+		},
+		{
+			name: "test vertical pattern - part 2",
+			args: args{
+				patterns[1],
+				1,
 			},
 			want: -1,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := findVerticalReflect(tt.args.pattern); got != tt.want {
+			if got := findVerticalReflect(tt.args.pattern, tt.args.wantedError); got != tt.want {
 				t.Errorf("verticalReflect() = %v, want %v", got, tt.want)
 			}
 		})
@@ -41,7 +60,8 @@ func Test_findVerticalReflect(t *testing.T) {
 func Test_findHorizontalReflect(t *testing.T) {
 	loadFile("test.txt")
 	type args struct {
-		pattern [][]rune
+		pattern     [][]rune
+		wantedError int
 	}
 	tests := []struct {
 		name string
@@ -49,23 +69,41 @@ func Test_findHorizontalReflect(t *testing.T) {
 		want int
 	}{
 		{
-			name: "test horizontal pattern",
+			name: "test horizontal pattern - part 1",
 			args: args{
-				pattern: patterns[0],
+				patterns[0],
+				0,
 			},
 			want: -1,
 		},
 		{
-			name: "test horizontal pattern",
+			name: "test horizontal pattern - part 1",
 			args: args{
-				pattern: patterns[1],
+				patterns[1],
+				0,
 			},
 			want: 4,
+		},
+		{
+			name: "test horizontal pattern - part 2",
+			args: args{
+				patterns[0],
+				1,
+			},
+			want: 3,
+		},
+		{
+			name: "test horizontal pattern - part 2",
+			args: args{
+				patterns[1],
+				1,
+			},
+			want: 1,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := findHorizontalReflect(tt.args.pattern); got != tt.want {
+			if got := findHorizontalReflect(tt.args.pattern, tt.args.wantedError); got != tt.want {
 				t.Errorf("findHorizontalReflect() = %v, want %v", got, tt.want)
 			}
 		})
