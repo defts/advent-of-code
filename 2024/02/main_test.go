4ec3dd2fb6b8943d128de94e134a7611c7da0f66
David Torres Dias <david@torresdias.fr>
2024-12-02 09:20:40 +0100

    fix: remove commeted tests

--- 2024/02/main_test.go
+++ 2024/02/main_test.go
@@ -4,24 +4,6 @@ import (
 	"testing"
 )
 
-// func Test_compute(t *testing.T) {
-// 	loadFile("test.txt")
-// 	type args struct {
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 		want int
-// 	}{}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			if got := compute(tt.args.line); got != tt.want {
-// 				t.Errorf("compute() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
-
 func Test_part1(t *testing.T) {
 	loadFile("test.txt")
 	type args struct {
